07789a6202bdce4b871afa9b83e89885e692f7bf
manya <manya@10.12.38.188>
2022-09-19 09:14:58 +0530

    BUUBLE SORTING IS THE BEST

---
+++ src/BubbleSort.java
@@ -0,0 +1,28 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] ={3,60,35,2,45,320,5};  
+        
+        System.out.println("Array Before Bubble Sort");  
+        for(int i=0; i < arr.length; i++){  
+                System.out.print(arr[i] + " ");  
+        }  
+        System.out.println();  
+		 int n = arr.length;  
+	        int temp = 0;  
+	         for(int i=0; i < n; i++){  
+	                 for(int j=1; j < (n-i); j++){  
+	                          if(arr[j-1] > arr[j]){  
+	                                 //swap elements  
+	                                 temp = arr[j-1];  
+	                                 arr[j-1] = arr[j];  
+	                                 arr[j] = temp;  
+	                         }  
+	                          
+	                 }  
+	         }
+	}
+
+}
